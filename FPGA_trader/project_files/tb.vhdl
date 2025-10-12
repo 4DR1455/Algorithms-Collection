cb399af73f80648ddf63a5976152fcc6d0c62641
Adrià <108619831+4DR1455@users.noreply.github.com>
2025-10-13 00:29:27 +0200

    Update tb.vhdl

--- FPGA_trader/project_files/tb.vhdl
+++ FPGA_trader/project_files/tb.vhdl
@@ -5,51 +5,81 @@ use ieee.numeric_std.all;
 entity tb_Sell_decider is
 end entity;
 
+
 architecture tb of tb_Sell_decider is
-    signal Top_win, Acc_change, Top_loss : signed(15 downto 0) := (others => '0');
+    signal Top_win       : signed(15 downto 0) := (others => '0');
+    signal Top_loss      : signed(15 downto 0) := (others => '0');
+    signal Acc_change_in : signed(15 downto 0) := (others => '0');
+    signal Acc_change_out: signed(15 downto 0) := (others => '0');
+    signal change        : signed(15 downto 0) := (others => '0');
     signal GT_loss, LT_loss, EQ_loss : std_logic;
-    signal GT_win, LT_win, EQ_win : std_logic;
-    signal SF : std_logic;
-    signal BF : std_logic;
+    signal GT_win,  LT_win,  EQ_win  : std_logic;
+    signal SF, BF        : std_logic;
+    signal clk      : std_logic := '0';
+    signal rst_reg, rst_add  : std_logic := '1';
 begin
-    uut1: entity work.cmp16_win
+    uut_cmp_win: entity work.cmp16_win
         port map (
             Top_win => Top_win,
-            Acc_change => Acc_change,
+            Acc_change => Acc_change_out,
             LT_win => LT_win,
             GT_win => GT_win,
             EQ_win => EQ_win
         );
-    uut2: entity work.cmp16_loss
+    uut_cmp_loss: entity work.cmp16_loss
         port map (
             Top_loss => Top_loss,
-            Acc_change => Acc_change,
+            Acc_change => Acc_change_out,
             LT_loss => LT_loss,
             GT_loss => GT_loss,
             EQ_loss => EQ_loss
         );
-    uut3: entity work.sell_flag
+    uut_sf: entity work.sell_flag
         port map (
             GT_win => GT_win,
             EQ_win => EQ_win,
             SF => SF
         );
-    uut4: entity work.buy_flag
+    uut_lf: entity work.buy_flag
         port map (
             LT_loss => LT_loss,
             EQ_loss => EQ_loss,
             BF => BF
         );
+    uut_add: entity work.add
+        port map (
+            clk => clk,
+            rst => rst_add,
+            change => change,
+            Acc_change_in => Acc_change_in,
+            Acc_change_out => Acc_change_out
+        );
+    uut_reg16: entity work.reg16
+        port map (
+            clk => clk,
+            rst => rst_reg,
+            din => Acc_change_out,
+            dout => Acc_change_in
+        );
 
-    stim: process
+    main_stim: process
     begin
+        wait for 3 ns;
         Top_win <= to_signed(7, 16);  Top_loss <= to_signed(-2, 16);
-        Acc_change <= to_signed(5, 16);  wait for 10 ns;  
-        Acc_change <= to_signed(2, 16);  wait for 10 ns;  
-        Acc_change <= to_signed(10, 16);  wait for 10 ns; 
-        Acc_change <= to_signed(-8, 16); wait for 10 ns;  
-        Acc_change <= to_signed(-4, 16);  wait for 10 ns;
-        Acc_change <= to_signed(5, 16); wait for 10 ns;
+        rst_reg <= '0'; rst_add <= '0';
+        change <= to_signed(5, 16);  wait for 15 ns;  
+        change <= to_signed(2, 16);  wait for 15 ns;  
+        change <= to_signed(10, 16);  wait for 15 ns; 
+        change <= to_signed(-8, 16); wait for 15 ns;  
+        change <= to_signed(-4, 16);  wait for 15 ns;
+        change <= to_signed(5, 16); wait for 15 ns;
         wait;
-    end process;
+    end process main_stim;
+    stim_clk: process
+    begin
+        while true loop
+            clk <= '0'; wait for 5 ns;
+            clk <= '1'; wait for 5 ns;
+        end loop;
+    end process stim_clk;
 end architecture;
