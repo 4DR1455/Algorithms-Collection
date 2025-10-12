fd328dbe547d91eb4bf4f47e00da19407726cbab
Adrià <108619831+4DR1455@users.noreply.github.com>
2025-10-12 22:28:09 +0200

    Update circuit.vhdl

--- FPGA_trader/project_files/circuit.vhdl
+++ FPGA_trader/project_files/circuit.vhdl
@@ -6,17 +6,17 @@ entity cmp16_win is
     port (
         Top_win   : in  signed(15 downto 0);
         Acc_change   : in  signed(15 downto 0);
-        EQ  : out std_logic;
-        GT  : out std_logic;
-        LT  : out std_logic
+        EQ_win  : out std_logic;
+        GT_win  : out std_logic;
+        LT_win  : out std_logic
     );
 end entity cmp16_win;
 
 architecture arch_cmp16_win of cmp16_win is
 begin
-    EQ <= '1' when Top_win = Acc_change else '0';
-    GT <= '1' when Top_win < Acc_change else '0';
-    LT <= '1' when Top_win > Acc_change else '0';
+    EQ_win <= '1' when Top_win = Acc_change else '0';
+    GT_win <= '1' when Top_win < Acc_change else '0';
+    LT_win <= '1' when Top_win > Acc_change else '0';
 end architecture arch_cmp16_win;
 
 
@@ -27,15 +27,15 @@ use ieee.numeric_std.all;
 
 entity sell_flag is
     port (
-        EQ : in std_logic;
-        GT : in std_logic;
+        EQ_win : in std_logic;
+        GT_win : in std_logic;
         SF : out std_logic
     );
 end entity sell_flag;
 
 architecture arch_sell_flag of sell_flag is
 begin
-    SF <= EQ or GT;
+    SF <= EQ_win or GT_win;
 end architecture arch_sell_flag;
 
 
@@ -44,22 +44,22 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity cmp16_loose is
+entity cmp16_loss is
     port (
-        Top_loose : in  signed(15 downto 0);
+        Top_loss : in  signed(15 downto 0);
         Acc_change : in  signed(15 downto 0);
-        EQ  : out std_logic;
-        GT  : out std_logic;
-        LT  : out std_logic
+        EQ_loss  : out std_logic;
+        GT_loss  : out std_logic;
+        LT_loss  : out std_logic
     );
-end entity cmp16_loose;
+end entity cmp16_loss;
 
-architecture arch_cmp16_loose of cmp16_loose is
+architecture arch_cmp16_loss of cmp16_loss is
 begin
-    EQ <= '1' when Top_loose = Acc_change else '0';
-    GT <= '1' when Top_loose < Acc_change else '0';
-    LT <= '1' when Top_loose > Acc_change else '0';
-end architecture arch_cmp16_loose;
+    EQ_loss <= '1' when Top_loss = Acc_change else '0';
+    GT_loss <= '1' when Top_loss < Acc_change else '0';
+    LT_loss <= '1' when Top_loss > Acc_change else '0';
+end architecture arch_cmp16_loss;
 
 
 
@@ -69,13 +69,13 @@ use ieee.numeric_std.all;
 
 entity buy_flag is
     port (
-        EQ : in std_logic;
-        LT : in std_logic;
+        EQ_loss : in std_logic;
+        LT_loss : in std_logic;
         BF : out std_logic
     );
 end entity buy_flag;
 
 architecture arch_buy_flag of buy_flag is
 begin
-    BF <= EQ or LT;
+    BF <= EQ_loss or LT_loss;
 end architecture arch_buy_flag;
