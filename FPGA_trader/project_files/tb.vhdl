f433c38457fcab91c59ef5a036f4423c696ffb8c
Adrià <108619831+4DR1455@users.noreply.github.com>
2025-10-12 22:28:30 +0200

    Update tb.vhdl

--- FPGA_trader/project_files/tb.vhdl
+++ FPGA_trader/project_files/tb.vhdl
@@ -6,49 +6,50 @@ entity tb_Sell_decider is
 end entity;
 
 architecture tb of tb_Sell_decider is
-    signal A, B, C : signed(15 downto 0) := (others => '0');
-    signal GT, LT, EQ : std_logic;
+    signal Top_win, Acc_change, Top_loss : signed(15 downto 0) := (others => '0');
+    signal GT_loss, LT_loss, EQ_loss : std_logic;
+    signal GT_win, LT_win, EQ_win : std_logic;
     signal SF : std_logic;
     signal BF : std_logic;
 begin
     uut1: entity work.cmp16_win
         port map (
-            Top_win => A,
-            Acc_change => B,
-            LT => LT,
-            GT => GT,
-            EQ => EQ
+            Top_win => Top_win,
+            Acc_change => Acc_change,
+            LT_win => LT_win,
+            GT_win => GT_win,
+            EQ_win => EQ_win
         );
-    uut2: entity work.cmp16_loose
+    uut2: entity work.cmp16_loss
         port map (
-            Top_loose => C,
-            Acc_change => B,
-            LT => LT,
-            GT => GT,
-            EQ => EQ
+            Top_loss => Top_loss,
+            Acc_change => Acc_change,
+            LT_loss => LT_loss,
+            GT_loss => GT_loss,
+            EQ_loss => EQ_loss
         );
     uut3: entity work.sell_flag
         port map (
-            GT => GT,
-            EQ => EQ,
+            GT_win => GT_win,
+            EQ_win => EQ_win,
             SF => SF
         );
     uut4: entity work.buy_flag
         port map (
-            LT => LT,
-            EQ => EQ,
+            LT_loss => LT_loss,
+            EQ_loss => EQ_loss,
             BF => BF
         );
 
     stim: process
     begin
-        A <= to_signed(7, 16);  C <= to_signed(2, 16);
-        B <= to_signed(5, 16);  wait for 10 ns;  
-        B <= to_signed(2, 16);  wait for 10 ns;  
-        B <= to_signed(10, 16);  wait for 10 ns; 
-        B <= to_signed(-8, 16); wait for 10 ns;  
-        B <= to_signed(-4, 16);  wait for 10 ns;
-        B <= to_signed(5, 16); wait for 10 ns;
+        Top_win <= to_signed(7, 16);  Top_loss <= to_signed(-2, 16);
+        Acc_change <= to_signed(5, 16);  wait for 10 ns;  
+        Acc_change <= to_signed(2, 16);  wait for 10 ns;  
+        Acc_change <= to_signed(10, 16);  wait for 10 ns; 
+        Acc_change <= to_signed(-8, 16); wait for 10 ns;  
+        Acc_change <= to_signed(-4, 16);  wait for 10 ns;
+        Acc_change <= to_signed(5, 16); wait for 10 ns;
         wait;
     end process;
 end architecture;
