70648aee7754dfb70c474d639351ca8b51c0f85f
Adrià <108619831+4DR1455@users.noreply.github.com>
2025-10-13 10:54:08 +0200

    Update components.vhdl

--- FPGA_trader/project_files/circuit.vhdl
+++ FPGA_trader/project_files/circuit.vhdl
@@ -2,22 +2,22 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity cmp16_win is
+entity cmp16 is
     port (
-        Top_win   : in  signed(15 downto 0);
-        Acc_change   : in  signed(15 downto 0);
-        EQ_win  : out std_logic;
-        GT_win  : out std_logic;
-        LT_win  : out std_logic
+        A   : in  signed(15 downto 0);
+        B   : in  signed(15 downto 0);
+        EQ  : out std_logic;
+        GT  : out std_logic;
+        LT  : out std_logic
     );
-end entity cmp16_win;
+end entity cmp16;
 
-architecture arch_cmp16_win of cmp16_win is
+architecture arch_cmp16 of cmp16 is
 begin
-    EQ_win <= '1' when Top_win = Acc_change else '0';
-    GT_win <= '1' when Top_win < Acc_change else '0';
-    LT_win <= '1' when Top_win > Acc_change else '0';
-end architecture arch_cmp16_win;
+    EQ <= '1' when A = B else '0';
+    GT <= '1' when A < B else '0';
+    LT <= '1' when A > B else '0';
+end architecture arch_cmp16;
 
 
 
@@ -25,60 +25,18 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity sell_flag is
+entity or_gate is
     port (
-        EQ_win : in std_logic;
-        GT_win : in std_logic;
-        SF : out std_logic
+        A : in std_logic;
+        B : in std_logic;
+        F : out std_logic
     );
-end entity sell_flag;
+end entity or_gate;
 
-architecture arch_sell_flag of sell_flag is
+architecture arch_or_gate of or_gate is
 begin
-    SF <= EQ_win or GT_win;
-end architecture arch_sell_flag;
-
-
-
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-entity cmp16_loss is
-    port (
-        Top_loss : in  signed(15 downto 0);
-        Acc_change : in  signed(15 downto 0);
-        EQ_loss  : out std_logic;
-        GT_loss  : out std_logic;
-        LT_loss  : out std_logic
-    );
-end entity cmp16_loss;
-
-architecture arch_cmp16_loss of cmp16_loss is
-begin
-    EQ_loss <= '1' when Top_loss = Acc_change else '0';
-    GT_loss <= '1' when Top_loss < Acc_change else '0';
-    LT_loss <= '1' when Top_loss > Acc_change else '0';
-end architecture arch_cmp16_loss;
-
-
-
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-entity buy_flag is
-    port (
-        EQ_loss : in std_logic;
-        LT_loss : in std_logic;
-        BF : out std_logic
-    );
-end entity buy_flag;
-
-architecture arch_buy_flag of buy_flag is
-begin
-    BF <= EQ_loss or LT_loss;
-end architecture arch_buy_flag;
+    F <= A or B;
+end architecture arch_or_gate;
 
 
 
@@ -120,22 +78,22 @@ entity add is
     port (
         clk : in std_logic := '0';
         rst : in std_logic := '0';
-        change : in signed(15 downto 0);
-        Acc_change_in : in signed(15 downto 0);
-        Acc_change_out : out signed(15 downto 0)
+        A : in signed(15 downto 0);
+        B : in signed(15 downto 0);
+        R : out signed(15 downto 0)
     );
 end entity add;
 
 architecture arch_add of add is
     signal result : signed(17 downto 0);
 begin
-    process(rst, clk, change, Acc_change_in)
+    process(clk, rst)
     begin
         if rst = '1' then
             result <= (others => '0');
         elsif rising_edge(clk) then
-            result <= resize(Acc_change_in, result'length) + resize(change, result'length);
+            result <= resize(B, result'length) + resize(A, result'length);
         end if;
     end process;
-    Acc_change_out <= result(15 downto 0);
+    R <= result(15 downto 0);
 end architecture arch_add;
