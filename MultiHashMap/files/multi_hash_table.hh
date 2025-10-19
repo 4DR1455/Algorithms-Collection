2f5af83d29edd412ecb69f52fa3e544474530618
Adrià <108619831+4DR1455@users.noreply.github.com>
2025-10-19 12:39:03 +0200

    Update multi_hash_table.hh

--- MultiHashMap/files/multi_hash_table.hh
+++ MultiHashMap/files/multi_hash_table.hh
@@ -1,4 +1,4 @@
-#include<list>
+#include<vector>
 #include"basic_hash_table.hh"
 
 template<typename K, typename V>
@@ -9,40 +9,27 @@ public:
         tables.push_back(HashTable<K,V>(_capacity));
     }
     void insert_or_update(const K& key, const V& value) {
-        int i = 0;
-        for (auto& t : tables) {
-            int last_size = t.size();
-            if (t.insert_or_update(key, value)) {
-                if (last_size + 1 == t.size()) _size++;
-                return;
-            }
-            i++;
-        }
-        tables.push_back(HashTable<K,V>(_capacity));
-        tables.back().insert_or_update(key, value);
+        int n_h_m = key/_capacity;
+        if (n_h_m >= tables.size()) expand_tables(n_h_m);
+        tables[n_h_m].insert_or_update(key, value);
         _size++;
     }
     void erase(const K& key) {
-        for (auto it = tables.begin(); it != tables.end(); ++it) {
-            bool removed = it->erase(key);
-            if (removed) {
-                --_size;
-                if (it->size() == 0) {
-                    it=tables.erase(it);
-                }
-                return;
-            }
-        }
+        tables[key/_capacity].erase(key);
+        _size--;
     }
     std::optional<V> find(const K& key) {
-        for (auto& t : tables) {
-            auto v = t.get(key);
-            if (v != std::nullopt) return v;
-        }
+        std::optional<V> r = tables[key/_capacity].get(key);
+        if (r.has_value()) return r;
         return std::nullopt;
     }
 private:
-    std::list<HashTable<K,V>> tables;
+void expand_tables(int n_h_m) {
+        while (tables.size() <= n_h_m) {
+            tables.push_back(HashTable<K,V>(_capacity));
+        }
+    }
+    std::vector<HashTable<K,V>> tables;
     int _size;
     int _capacity;
 };
