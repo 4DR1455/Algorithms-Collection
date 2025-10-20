63ea99cf3439c318c67069cb72e1ae73fba17c34
Adrià <108619831+4DR1455@users.noreply.github.com>
2025-10-20 10:07:34 +0200

    Update multi_hash_table.hh

--- MultiHashMap/files/multi_hash_table.hh
+++ MultiHashMap/files/multi_hash_table.hh
@@ -9,21 +9,28 @@ public:
         expand_tables(_capacity - 1);
     }
     void insert_or_update(const K& key, const V& value) {
-        int n_h_m = key/_capacity;
+        std::size_t idx = index_for(key);
+        int n_h_m = idx/_capacity;
         if (n_h_m >= tables.size()) expand_tables(n_h_m);
-        tables[n_h_m].insert_or_update(key, value);
+        tables[n_h_m].insert_or_update(idx%_capacity, value);
         _size++;
     }
     void erase(const K& key) {
-        tables[key/_capacity].erase(key);
+        std::size_t idx = index_for(key);
+        tables[idx/_capacity].erase(idx%_capacity);
         _size--;
     }
     std::optional<V> find(const K& key) {
-        std::optional<V> r = tables[key/_capacity].get(key);
+        std::size_t idx = index_for(key);
+        std::optional<V> r = tables[idx/_capacity].get(idx%_capacity);
         if (r.has_value()) return r;
         return std::nullopt;
     }
 private:
+std::size_t index_for(const K& key) const {
+        return std::hash<K>{}(key);
+    }
+
 void expand_tables(int n_h_m) {
         while (tables.size() <= n_h_m) {
             tables.push_back(HashTable<K,V>(_capacity));
