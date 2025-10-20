580aa89bd1793fdf311750efece1e1dc1adad41f
Adrià <108619831+4DR1455@users.noreply.github.com>
2025-10-20 10:07:09 +0200

    Update basic_hash_table.hh

--- MultiHashMap/files/basic_hash_table.hh
+++ MultiHashMap/files/basic_hash_table.hh
@@ -19,46 +19,35 @@ public:
 
     // Retorna true si s'insereix; retorna false si la posició hash està ocupada.
     // Si la mateixa clau ja hi és, insert també retorna false (no actualitza).
-    bool insert_or_update(const K& key, const V& value) {
-        std::size_t idx = index_for(key);
-        if (table[idx].has_value() && table[idx]->first != key) return false;
-        if (table[idx].has_value() && table[idx]->first == key) {
-            table[idx]->second = value;
+    bool insert_or_update(const std::size_t idx, const V& value) {
+        if (table[idx].has_value()) {
+            table[idx] = value;
             return true;
         }
-        table[idx] = std::make_pair(key, value);
+        table[idx] = value;
         _size++;
         return true;
     }
 
     // Retorna optional amb el valor si la clau és a la taula.
-    std::optional<V> get(const K& key) const {
-        std::size_t idx = index_for(key);
+    std::optional<V> get(const std::size_t idx) const {
         if (!table[idx].has_value()) return std::nullopt;
-        if (table[idx]->first == key) return table[idx]->second;
-        return std::nullopt; // col·lisió no resolta
+        return table[idx];
     }
 
     // Retorna true si la clau existia i s'ha esborrat.
-    bool erase(const K& key) {
-        std::size_t idx = index_for(key);
+    bool erase(const std::size_t idx) {
         if (!table[idx].has_value()) return false;
-        if (table[idx]->first == key) {
-            table[idx].reset();
-            return true;
-        }
-        return false;
+        table[idx] = std::nullopt;
+        _size--;
+        return true;
     }
 
     int size() const {return _size;}
     std::size_t capacity() const { return cap; }
 
 private:
-    std::size_t index_for(const K& key) const {
-        return std::hash<K>{}(key) % cap;
-    }
-
     int _size;
     std::size_t cap;
-    std::vector<std::optional<std::pair<K, V>>> table;
+    std::vector<std::optional<V>> table;
 };
